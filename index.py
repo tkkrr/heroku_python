e9b92db8f7837c0aa747525dea2154af72040c38
tkkrr <reioffitial@gmail.com>
2019-12-01 00:37:19 +0900

    initial commit

---
+++ index.py
@@ -0,0 +1,27 @@
+from flask import Flask, url_for, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return '''
+    <link rel="stylesheet" type="text/css" href="static/style.css">
+    <h1>Hello World!!</h1>
+    '''
+
+@app.route('/hello')
+def hello(methods = ["GET"]):
+    name = request
+    print(name)
+    # name = request.args.get("name")
+    return '''
+    <p>Hello?</p>
+    '''
+
+@app.route('/hello/<int:username>')
+def hello_name(username):
+    return str(username)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
